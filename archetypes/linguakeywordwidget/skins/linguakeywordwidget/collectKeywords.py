0aaf48984c18f906f52b324469e62bb6bf396709
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-01-10 14:57:26 +0100

    Add a workaround to make collectKeyword script support field added with schemaextender

--- archetypes/linguakeywordwidget/skins/linguakeywordwidget/collectKeywords.py
+++ archetypes/linguakeywordwidget/skins/linguakeywordwidget/collectKeywords.py
@@ -7,14 +7,19 @@
 ##bind subpath=traverse_subpath
 ##parameters=name, index, vocab_source='portal_catalog'
 
-REQUEST=context.REQUEST
+REQUEST = context.REQUEST
 
+#patch to support keyword widget using schema extender
+#let integrator work around that issue
+if index is None:
+    index = name
 
 ## With the advent of multi-cataloging we need to pass an optional
 ## catalog id to use for the widget.
 
 allowed, enforce = context.Vocabulary(name)
 catalog = getattr(container, vocab_source)
+
 previous = catalog.uniqueValuesFor(index)
 
 if enforce:
